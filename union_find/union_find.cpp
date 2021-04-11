61051b33a90b26c2e52f49259f3ab33e1cc53a4a
siuwhat <1290634672@qq.com>
2021-04-11 22:10:53 +0800

    second refined

--- union_find/union_find.cpp
+++ union_find/union_find.cpp
@@ -4,21 +4,34 @@ using namespace std;
 
  class Union {
 public:
-	Union(size_t size = 10) :SIZE(size) { parent = vector<int>(SIZE); for (auto i = 0; i < SIZE; ++i)parent[i]=i; }	
+	Union(size_t size = 10) :SIZE(size) { parent = vector<int>(SIZE); weight = vector<int>(SIZE); for (auto i = 0; i < SIZE; ++i) { parent[i] = i; weight[i] = 0; } }
 	void union_element(int source, int destination);
 	bool if_connected(int first,int second);
 	size_t count() { return SIZE; }
 	int find();
 	void traverse() { for (auto i = 0; i < SIZE; ++i) cout << parent[i] << ends; cout << endl; }
+	vector<int>weight;
 private:
 	size_t SIZE;
 	vector<int>parent;
+	
 };
  void Union::union_element(int destination, int source) {
 	 if (!if_connected(destination, source)) {
 		 while (parent[source] != source)source = parent[source];
 		 while (parent[destination] != destination)destination = parent[destination];
-		 parent[destination] = source;
+		 if (weight[source] >= weight[destination])
+		 {
+			 parent[destination] = source;	
+			 weight[source]++;
+			 weight[source]+=weight[destination];
+		 }
+		 else
+		 {
+			 parent[source] = destination;
+			 weight[destination]++;
+			 weight[destination]+=weight[source];
+		 }
 	 }
 	 traverse();
  }
@@ -29,16 +42,16 @@ private:
 int main()
 {
 	Union a;
-	a.union_element(4,3);
+	a.union_element(3,4);
 	a.union_element(3, 8);
-	a.union_element(6, 5);
+	a.union_element(5, 6);
 	a.union_element(9, 4);
-	a.union_element(2, 1);
-	a.union_element(5, 0);
+	a.union_element(1, 2);
+	a.union_element(0, 5);
 	a.union_element(7, 2);
-	a.union_element(6, 1);
+	a.union_element(1, 6);
 	a.union_element(7, 3);
-
+	
 	
 
 	
