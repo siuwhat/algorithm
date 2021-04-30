86dd286f4cb8f3a13fe30115ecb3ac1924e809b1
siuwhat <1290634672@qq.com>
2021-04-30 15:35:32 +0800

    change

--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -2,59 +2,56 @@
 #include <iostream>
 #include<vector>
 #include<test_if_sorted.hpp>
+#include<random>
 using namespace std;
-void merge(int* a, int* aux, int lo, int mid, int hi) {
+random_device r;
+void traverse(vector<int>& a) { for (auto i : a)cout << i << ends; cout << endl; }
+void merge(vector<int>&a, vector<int>& aux, int lo, int mid, int hi) {
     int k;
-    for (k = 0; k <= hi; k++) {
+    for (k = lo; k <= hi; k++) {
         aux[k] = a[k];
-    }
+    }cout << "回合内:";
+    traverse(aux);
+    traverse(a);
+    cout << "回合结束" << endl;
     int i = lo, j = mid + 1;
     for (k = lo; k <= hi; k++) {
-        if (i > mid) {
-            a[k] = aux[j];
-            j = j + 1;
-        }
-        else {
-            if (j > hi) {
-                a[k] = aux[i];
-                i = i + 1;
-            }
-            else {
-                if (aux[j] < aux[i]) {
-                    a[k] = aux[j];
-                    j = j + 1;
-                }
-                else {
-                    a[k] = aux[i];
-                    i = i + 1;
-                }
-            }
-        }
-
+        cout << endl;
+        cout << "k: "<<k << ends;
+        cout << endl;
+        if (i > mid) 
+            a[k] = aux[j++];
+        else if(j>hi)  
+            a[k] = aux[i++];
+        else if (aux[j] < aux[i]) 
+            a[k] = aux[j++];
+        else 
+            a[k] = aux[i++];
     }
 }
 
-void sort(int* a) {
-    int lo, N = 10;
-    int aux[10];
-    int sz;
-    for (sz = 1; sz < N; sz = sz + 1) {
-        for (lo = 0; lo < N - sz; lo = lo + sz + sz) {
-            merge(a, aux, lo, lo + sz - 1, min(lo + sz + sz - 1, N - 1));
+void sort(vector<int>&a) {
+    const int N = a.size();
+    vector<int> aux(N, 0);
+    for (int sz = 1; sz < N; sz+=sz) {
+        for (int lo = 0; lo < N-sz ; lo += 2*sz ) {
+            cout << "lo: " << lo << " mid: " << lo + sz - 1 << " end: " << min(lo + 2 * sz - 1, N - 1)<<endl;
+            merge(a, aux, lo, lo + sz - 1, min(lo + 2*sz - 1, N - 1));
+            traverse(a);
         }
     }
 }
 
 int main()
 {
-    int i;
-    int a[10] = { 4, 3, 0, 2, 1, 2, 3, 4, 9, 1 };
-    int aux[10];
+    vector<int> a;
+    for (auto i = 0; i < 14; ++i) { 
+        //a.emplace_back(r() % 100); 
+        a.emplace_back(14 - i);
+    };
 
+    traverse(a);
     sort(a);
-    for (i = 0; i < 10; i++) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
+    traverse(a);
 }
 
